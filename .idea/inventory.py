8b4bbed9b966a090032fd80a5194f69f477bdea2
varmeya <varmeya@gmail.com>
2021-01-07 21:56:18 +0100

    start

---
+++ .idea/inventory.py
@@ -0,0 +1,22 @@
+#wprowadzanie imienia i nazwiska bohatera
+#wybór klasy z listy > jesli nie chcesz wybrac klasy to wybiera randomowo
+#dodawanie i usuwanie przedmiotow z ekwipunku > postac musi miec przynajmniej 1 przedmiot typu bron i typu zbroja
+#statystyki postaci- sila, intelekt, stamina, zwinnosc - statystyki zalezne od klasy i mozliwe do rozdawania po lvl up
+#ekwipunek ma wymagana wartosc statystyk do  uzywania np 15 sily dla longsworda
+#maksymalny udzwig postaci- kazdy przedmiot ma swoja wage
+#walki na arenie - przeciwnicy zadaja okreslona ilsoc dmg fizycznego i magizcnego w zaleznosci od swoich stat
+#staty sa zalezne od lvl potwora i sa porownywane ze statami bohatera, wstepnie mechanizm wiecej sily lub inta wygrywa z przelicznikiem dla danych stat, zwinnosc od dodgle
+
+print('Witaj w świecie Wojennego Rzemiosła!')
+print('Wprowadź imię swojego bohatera:')
+yourhero= input()
+
+print(f" w porządku {yourhero} tutaj znajduje sie wyposazenie twojego czempiona azerytu: ")
+
+inventory=("Miecz",
+           "tarcza",
+           "czerwone buty szybkości",
+           "mikstura many")
+print(inventory)
+
+#print(type(twojbohater))
